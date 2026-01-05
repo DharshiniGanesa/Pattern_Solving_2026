61fcfdacf7d9a1831b1eabe9dd127b2642198943
Dharshini Ganesa <dharshiniganesa07@gmail.com>
2026-01-05 20:40:45 +0530

    Added Jan Week2 Day2

---
+++ pattern_solving_jan2026/src/com/jan_week2/Day2_AlphabetPyramid.java
@@ -0,0 +1,31 @@
+package com.jan_week2;
+import java.util.Scanner;
+
+public class Day2_AlphabetPyramid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = scan.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 0; j < i; j++) {
+                System.out.print(" " + ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = 'A';
+            for (int j = 0; j < i; j++) {
+                System.out.print(" " + ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
---
+++ pattern_solving_jan2026/src/com/jan_week2/Day2_DiamondofAlphabet.java
@@ -0,0 +1,38 @@
+package com.jan_week2;
+import java.util.Scanner;
+
+public class Day2_DiamondofAlphabet {
+	public static void main(String[] args) {
+	     Scanner scan = new Scanner(System.in);
+			
+			System.out.println("Enter a number: ");
+			int n = scan.nextInt();
+			
+			
+			for(int i =0;i<=n;i++) {
+				for(int k=0;k<=n-i;k++) {
+					System.out.print(" ");
+				}
+				char ch ='A';
+				for(int j=0;j<i;j++) {
+					System.out.print(" "+ch);
+					ch++;
+				}
+				System.out.println("");
+				
+			}
+			for(int i =n-1;i>=0;i--){
+				for(int k=0;k<=n-i;k++) {
+					System.out.print(" ");
+				}
+				char ch ='A';
+				for(int j=0;j<i;j++) {
+					System.out.print(" "+ch);
+					ch++;
+				}
+				System.out.println("");
+			}
+			
+			scan.close();
+		}
+}
